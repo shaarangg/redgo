a33c3817a9cec6d5c8f604c0e088703aa09ec551
Shaarang Singh <shaarangg.singh@gmail.com>
2024-12-28 15:08:48 +0530

    feat: Start Redis Server

--- main.go
+++ main.go
@@ -1,78 +1,38 @@
 package main
 
 import (
-	"log"
-	"log/slog"
+	"fmt"
 	"net"
 )
 
-const defaultListenAddr = ":5001"
-
-type Config struct {
-	ListenAddr string
-}
-
-type Server struct {
-	Config
-	peers    map[*Peer]bool
-	ln       net.Listener
-	addPerCh chan *Peer
-	quitCh   chan struct{}
-}
-
-func NewServer(cfg Config) *Server {
-	if len(cfg.ListenAddr) == 0 {
-		cfg.ListenAddr = defaultListenAddr
-	}
-	return &Server{
-		Config:   cfg,
-		peers:    make(map[*Peer]bool),
-		addPerCh: make(chan *Peer),
-		quitCh:   make(chan struct{}),
-	}
-}
-
-func (s *Server) Start() error {
-	ln, err := net.Listen("tcp", s.ListenAddr)
-	if err != nil {
-		return err
-	}
-	s.ln = ln
-	go s.loop()
-	slog.Info("server running", "listenAddr", s.ListenAddr)
-	return s.acceptLoop()
-}
-
-func (s *Server) loop() {
-	for {
-		select {
-		case <-s.quitCh:
-			return
-		case peer := <-s.addPerCh:
-			s.peers[peer] = true
-		}
-	}
-}
-
-func (s *Server) acceptLoop() error {
-	for {
-		conn, err := s.ln.Accept()
-		if err != nil {
-			slog.Error("accept error", "err", err)
-			continue
-		}
-		go s.handleConn(conn)
-	}
-}
-
-func (s *Server) handleConn(conn net.Conn) {
-	peer := NewPeer(conn)
-	s.addPerCh <- peer
-	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
-	go peer.readLoop()
-}
-
 func main() {
-	server := NewServer(Config{})
-	log.Fatal(server.Start())
+	fmt.Println("Listening on port :6379")
+
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(value)
+
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
